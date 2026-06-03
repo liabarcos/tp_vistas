f2d8f2c7091fa6efa73991ffcf198c00194421b4
liabarcos <liabarcos454@gmail.com>
2026-06-03 16:06:21 -0300

    Feat: Creacion de array en productos y cambio de Header

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('titulo', 'Inicio')
+@section('contenido')
+    <h2>Página de inicio</h2>
+    <p>Bienvenido a nuestra primera aplicación con Laravel y Blade.</p>
+    <p>Esta página usa un layout general</p>
+@endsection
---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('titulo', 'Nosotros')
+@section('contenido')
+    <h2>Página de Nosotros</h2>
+    <p>Bienvenidos a mi app</p>
+@endsection
